17a5012bf82cdd54abbbcd4fa9d8cea85216bdc8
Axenntio <axenntio@live.com>
2022-09-11 21:32:47 +0200

    Prepare for ruby binding (facing issue with object)

--- inc/Window.hpp
+++ inc/Window.hpp
@@ -1,17 +1,23 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include <mruby/compile.h>
 
 class Window : public sf::Drawable, public sf::Transformable {
 public:
 	Window(unsigned int width, unsigned int height, const std::vector<sf::Color>& palette);
+	~Window();
 
 	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 
 private:
+	mrb_value mrubyLine(mrb_state *mrb, mrb_value self);
+
 	sf::RenderTexture _texture;
 	std::vector<sf::Color> _palette;
 	unsigned int _width;
 	unsigned int _height;
 	sf::Vector2u _position;
+
+	mrb_state* _mrb;
 };
--- src/Window.cpp
+++ src/Window.cpp
@@ -5,6 +5,13 @@ Window::Window(unsigned int width, unsigned int height, const std::vector<sf::Co
 	this->_palette = palette;
 	this->_texture.create(this->_width, this->_height);
 	this->_texture.clear(this->_palette[3]);
+	this->_mrb = mrb_open();
+	// mrb_define_method(this->_mrb, this->_mrb->object_class, "line", , MRB_ARGS_NONE());
+}
+
+Window::~Window()
+{
+	mrb_close(this->_mrb);
 }
 
 void Window::draw(sf::RenderTarget& target, sf::RenderStates states) const
@@ -16,4 +23,9 @@ void Window::draw(sf::RenderTarget& target, sf::RenderStates states) const
 	sf::Sprite sprite(this->_texture.getTexture());
 
 	target.draw(sprite, states);
+}
+
+mrb_value Window::mrubyLine(mrb_state* mrb, mrb_value self)
+{
+	return mrb_nil_value();
 }
