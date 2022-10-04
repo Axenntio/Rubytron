0ad178c1d50f57f1a5bbc0b16148c891d4df29f4
Axenntio <axenntio@live.com>
2022-10-04 22:05:24 +0200

    Ignore mruby module since we add a custom config

--- programs/editor.rb
+++ programs/editor.rb
@@ -4,245 +4,251 @@ FONT_HEIGH = 6.freeze
 $editor = nil
 
 class Window
-	def self.resize_event(new_width, new_height)
-		$editor.update_show_line(new_width, new_height)
-	end
+  def self.resize_event(new_width, new_height)
+    $editor.update_show_line(new_width, new_height)
+  end
 
-	def self.text_event(char)
-        $editor.add_char(char)
-	end
+  def self.text_event(char)
+    $editor.add_char(char)
+  end
 
-    def self.key_press_event(key)
-        $editor.parse_key(key)
-	end
+  def self.key_press_event(key)
+    $editor.parse_key(key)
+  end
 end
 
 class Vector
-    attr_accessor :x
-    attr_accessor :y
+  attr_accessor :x
+  attr_accessor :y
 
-    def initialize(x, y)
-        self.x = x
-        self.y = y
-    end
+  def initialize(x, y)
+    self.x = x
+    self.y = y
+  end
 
-    def +(other)
-		Vector.new(self.x + other.x, self.y + other.y)
-	end
+  def +(other)
+    Vector.new(self.x + other.x, self.y + other.y)
+  end
 
-    def -(other)
-		Vector.new(self.x - other.x, self.y - other.y)
-	end
+  def -(other)
+    Vector.new(self.x - other.x, self.y - other.y)
+  end
 
-    def to_s
-        "{x: #{self.x}, y: #{self.y}}"
-    end
+  def to_s
+    "{x: #{self.x}, y: #{self.y}}"
+  end
 end
 
 class Editor
-    attr_accessor :cursor
+  attr_accessor :cursor
 
-    def initialize(path)
-        @path = path
-        @cursor = Vector.new(0, 0)
-        @code_shift = Vector.new(0, 0)
-        @file_content = File.open(@path, 'r') do |file|
-            file.read.split("\n")
-        end
-        @side_bar_width = @file_content.count.to_s.length
-        self.update_show_line(Window.width, Window.height)
+  def initialize(path)
+    @path = path
+    @cursor = Vector.new(0, 0)
+    @code_shift = Vector.new(0, 0)
+    @file_content = File.open(@path, 'r') do |file|
+      file.read.split("\n")
     end
+    @side_bar_width = @file_content.count.to_s.length
+    self.update_show_line(Window.width, Window.height)
+  end
 
-    def syntax_color(word)
-        syntaxes = {
-            1 => ['nil'],
-            2 => ['class', 'def', 'end', 'do', 'if'],
-            3 => ['/^[A-Z_]*$/'],
-            9 => ['/^*.\\(/']
-        }
-        syntaxes.each do |color, syntax|
-            return color if syntax.include?(word)
+  def syntax_color(word)
+    syntaxes = {
+      1 => [/^[A-Z_]+$/],
+      2 => ['module', 'class', 'def', 'end', 'do', 'if'],
+      3 => [/^[A-Z]{1}[a-z_]*$/],
+      # 9 => [/^[A-Za-z_]*\(/], # method
+      10 => [/^\d+$/],
+      12 => [/^[@:\$][A-Za-z_]+$/, 'nil', 'self', 'new', 'attr_accessor', 'attr_reader', 'attr_writer'],
+    }
+    syntaxes.each do |color, syntax|
+      syntax.each do |pattern|
+        if (pattern.is_a?(String) && pattern == word) ||
+           (pattern.is_a?(Regexp) && pattern =~ word)
+          return color
         end
-        7
+      end
     end
+    7
+  end
 
-    def update_show_line(width, height)
-        @show_line = height / FONT_HEIGH + 1
-        @show_char = width / FONT_WIDTH + 1 - @side_bar_width
-        Window.title = "#{@path} - #{width}x#{height}"
+  def update_show_line(width, height)
+    @show_line = height / FONT_HEIGH + 1
+    @show_char = width / FONT_WIDTH + 1 - @side_bar_width
+    Window.title = "#{@path} - #{width}x#{height}"
+  end
+
+  def showable_content_postition
+    Vector.new(@side_bar_width, 0)
+  end
+
+  def showable_content
+    @file_content[@code_shift.y, @show_line].map do |line|
+      if line.length >= @code_shift.x
+        line[@code_shift.x, @show_char]
+      else
+        ''
+      end
     end
+  end
 
-    def showable_content_postition
-        Vector.new(@side_bar_width, 0)
+  def content_lines
+    ((@code_shift.y + 1)..([@code_shift.y + @show_line, @file_content.count].min))
+  end
+
+  def showable_cursor
+    @cursor - @code_shift + self.showable_content_postition
+  end
+
+  def add_char(char)
+    if char.between?(32, 126) || char == 9 # tab
+      @file_content[@cursor.y].insert(@cursor.x, char.chr)
+      @cursor.x += 1
     end
+    self.update_scope
+  end
 
-    def showable_content
-        @file_content[@code_shift.y, @show_line].map do |line|
-            if line.length >= @code_shift.x
-                line[@code_shift.x, @show_char]
-            else
-                ''
-            end
-        end
-    end
-
-    def content_lines
-        ((@code_shift.y + 1)..([@code_shift.y + @show_line, @file_content.count].min))
-    end
-
-    def showable_cursor
-        @cursor - @code_shift + self.showable_content_postition
-    end
-
-    def add_char(char)
-        if char.between?(32, 126) || char == 9 # tab
-            @file_content[@cursor.y].insert(@cursor.x, char.chr)
-            @cursor.x += 1
-        end
-        self.update_scope
-    end
-
-    def parse_key(key)
-        case key
-        when 0 # ctrl + a
-            to_start_line if Window.key.include?(37)
-        when 4 # ctrl + e
-            to_end_line if Window.key.include?(37)
-        when 17 # ctrl + r
-            Window.reload if Window.key.include?(37)
-        when 18 # ctrl + s
-            save if Window.key.include?(37)
-        when 58 # Enter
-            new_line = @file_content[@cursor.y].slice!(@cursor.x, @file_content[@cursor.y].length - @cursor.x)
-            @cursor.y += 1
-            @cursor.x = 0
-            new_line ||= '' # Makes sure we have a string since spliting at last pos only provide `nil` value
-            @file_content.insert(@cursor.y, new_line)
-        when 59 # Backspace
-            if @cursor.x.positive?
-                @file_content[@cursor.y].slice!(@cursor.x - 1)
-                @cursor.x -= 1
-            elsif @cursor.y.positive?
-                @cursor.x = @file_content[@cursor.y - 1].length
-                @file_content[@cursor.y - 1] = @file_content[@cursor.y - 1] + @file_content[@cursor.y]
-                @file_content.delete_at(@cursor.y)
-                @cursor.y -= 1
-            end
-        when 61 # Page up
-            @cursor.y -= @show_line - 2
-        when 62 # Page down
-            @cursor.y += @show_line - 2
-        when 63 # End
-            to_end_line
-        when 64 # Home
-            to_start_line
-        when 66 # Del
-            if @file_content[@cursor.y].length > @cursor.x
-                @file_content[@cursor.y].slice!(@cursor.x)
-            else
-                @file_content[@cursor.y] = @file_content[@cursor.y] + @file_content[@cursor.y + 1]
-                @file_content.delete_at(@cursor.y + 1)
-            end
-        when 71 # Left
-            @cursor.x -= 1
-        when 72 # Right
-            @cursor.x += 1
-        when 73 # Down
-            @cursor.y -= 1
-            if @cursor.x > @file_content[@cursor.y].length
-                @cursor.x = @file_content[@cursor.y].length
-            end
-        when 74 # Up
-            @cursor.y += 1 if @cursor.y < (@file_content.count - 1)
-            if @cursor.x > @file_content[@cursor.y].length
-                @cursor.x = @file_content[@cursor.y].length
-            end
-        else
-            puts "Unhandled #{key}"
-        end
-        self.update_scope
-    end
-
-    def update_scope
-        @cursor.y = 0 if @cursor.y.negative?
-        @cursor.y = @file_content.count - 1 if @cursor.y >= (@file_content.count - 1)
-        if @cursor.x.negative?
-            @cursor.y -= 1
-            @cursor.x = 0
-            if @cursor.y >= 0
-                @cursor.x = @file_content[@cursor.y].length
-            end
-        end
-        if @cursor.x > @file_content[@cursor.y].length
-            if @cursor.y < (@file_content.count - 1)
-                @cursor.x = 0
-                @cursor.y += 1
-            else
-                @cursor.x = @file_content[@cursor.y].length
-            end
-        end
-        @cursor.y = 0 if @cursor.y.negative?
-        @cursor.y = @file_content.count - 1 if @cursor.y >= (@file_content.count - 1)
-
-        if @cursor.x >= (@code_shift.x + @show_char - 1)
-            @code_shift.x += @cursor.x - @show_char + 2 - @code_shift.x # Maybe unoptimized
-        end
-        if @cursor.x <= (@code_shift.x - 1)
-            @code_shift.x = @cursor.x
-        end
-        if @cursor.y >= (@code_shift.y + @show_line - 1)
-            @code_shift.y += @cursor.y - @show_line + 2 - @code_shift.y # Maybe unoptimized
-        end
-        if @cursor.y <= (@code_shift.y - 1)
-            @code_shift.y = @cursor.y
-        end
-        @side_bar_width = @file_content.count.to_s.length
-    end
-
-    def to_start_line
-        @cursor.x = 0
-        self.update_scope
-    end
-
-    def to_end_line
+  def parse_key(key)
+    case key
+    when 0 # ctrl + a
+      to_start_line if Window.key.include?(37)
+    when 4 # ctrl + e
+      to_end_line if Window.key.include?(37)
+    when 17 # ctrl + r
+      Window.reload if Window.key.include?(37)
+    when 18 # ctrl + s
+      save if Window.key.include?(37)
+    when 58 # Enter
+      new_line = @file_content[@cursor.y].slice!(@cursor.x, @file_content[@cursor.y].length - @cursor.x)
+      @cursor.y += 1
+      @cursor.x = 0
+      new_line ||= '' # Makes sure we have a string since spliting at last pos only provide `nil` value
+      @file_content.insert(@cursor.y, new_line)
+    when 59 # Backspace
+      if @cursor.x.positive?
+        @file_content[@cursor.y].slice!(@cursor.x - 1)
+        @cursor.x -= 1
+      elsif @cursor.y.positive?
+        @cursor.x = @file_content[@cursor.y - 1].length
+        @file_content[@cursor.y - 1] = @file_content[@cursor.y - 1] + @file_content[@cursor.y]
+        @file_content.delete_at(@cursor.y)
+        @cursor.y -= 1
+      end
+    when 61 # Page up
+      @cursor.y -= @show_line - 2
+    when 62 # Page down
+      @cursor.y += @show_line - 2
+    when 63 # End
+      to_end_line
+    when 64 # Home
+      to_start_line
+    when 66 # Del
+      if @file_content[@cursor.y].length > @cursor.x
+        @file_content[@cursor.y].slice!(@cursor.x)
+      else
+        @file_content[@cursor.y] = @file_content[@cursor.y] + @file_content[@cursor.y + 1]
+        @file_content.delete_at(@cursor.y + 1)
+      end
+    when 71 # Left
+      @cursor.x -= 1
+    when 72 # Right
+      @cursor.x += 1
+    when 73 # Down
+      @cursor.y -= 1
+      if @cursor.x > @file_content[@cursor.y].length
         @cursor.x = @file_content[@cursor.y].length
-        self.update_scope
+      end
+    when 74 # Up
+      @cursor.y += 1 if @cursor.y < (@file_content.count - 1)
+      if @cursor.x > @file_content[@cursor.y].length
+        @cursor.x = @file_content[@cursor.y].length
+      end
+    else
+      puts "Unhandled #{key}"
     end
+    self.update_scope
+  end
 
-    def save
-        File.open(@path, 'w') do |file|
-           file.write(@file_content.join("\n"))
-        end
+  def update_scope
+    @cursor.y = 0 if @cursor.y.negative?
+    @cursor.y = @file_content.count - 1 if @cursor.y >= (@file_content.count - 1)
+    if @cursor.x.negative?
+      @cursor.y -= 1
+      @cursor.x = 0
+      if @cursor.y >= 0
+        @cursor.x = @file_content[@cursor.y].length
+      end
     end
+    if @cursor.x > @file_content[@cursor.y].length
+      if @cursor.y < (@file_content.count - 1)
+        @cursor.x = 0
+        @cursor.y += 1
+      else
+        @cursor.x = @file_content[@cursor.y].length
+      end
+    end
+    @cursor.y = 0 if @cursor.y.negative?
+    @cursor.y = @file_content.count - 1 if @cursor.y >= (@file_content.count - 1)
+
+    if @cursor.x >= (@code_shift.x + @show_char - 1)
+      @code_shift.x += @cursor.x - @show_char + 2 - @code_shift.x # Maybe unoptimized
+    end
+    if @cursor.x <= (@code_shift.x - 1)
+      @code_shift.x = @cursor.x
+    end
+    if @cursor.y >= (@code_shift.y + @show_line - 1)
+      @code_shift.y += @cursor.y - @show_line + 2 - @code_shift.y # Maybe unoptimized
+    end
+    if @cursor.y <= (@code_shift.y - 1)
+      @code_shift.y = @cursor.y
+    end
+    @side_bar_width = @file_content.count.to_s.length
+  end
+
+  def to_start_line
+    @cursor.x = 0
+    self.update_scope
+  end
+
+  def to_end_line
+    @cursor.x = @file_content[@cursor.y].length
+    self.update_scope
+  end
+
+  def save
+    File.open(@path, 'w') do |file|
+      file.write(@file_content.join("\n"))
+    end
+  end
 end
 
 def init
-    $editor = Editor.new(Window.parameters[0])
+  $editor = Editor.new(Window.parameters[0])
 end
 
 def update
-    showable = 10
-    clear 0
-    editor_canvas = $editor.showable_content_postition
-    rect 0, 0, editor_canvas.x * FONT_WIDTH, Window.height, 1
-    y_offset = 1
-    $editor.content_lines.each do |number|
-        line = number.to_s
-        text 1 + (editor_canvas.x - line.length) * FONT_WIDTH, y_offset, line, 13, true
-        y_offset += FONT_HEIGH
+  showable = 10
+  clear 0
+  editor_canvas = $editor.showable_content_postition
+  rect 0, 0, editor_canvas.x * FONT_WIDTH, Window.height, 1
+  y_offset = 1
+  $editor.content_lines.each do |number|
+    line = number.to_s
+    text 1 + (editor_canvas.x - line.length) * FONT_WIDTH, y_offset, line, 13, true
+    y_offset += FONT_HEIGH
+  end
+  y_offset = 1
+  $editor.showable_content.each do |line|
+    parts = line.split(/([\s.,=+-]+)/)
+    part_offset = 0
+    parts.each do |part|
+       color = $editor.syntax_color(part)
+       text 1 + editor_canvas.x * FONT_WIDTH + part_offset, y_offset, part, color, true
+       part_offset += part.length * FONT_WIDTH
     end
-    y_offset = 1
-    $editor.showable_content.each do |line|
-        parts = line.split(' ') # TODO: Split by taking space in account
-        part_offset = 0
-        text 1 + editor_canvas.x * FONT_WIDTH, y_offset, line, 7, true
-        #parts.each do |part|
-        #    color = $editor.syntax_color(part)
-        #    text 1 + editor_canvas.x * FONT_WIDTH + part_offset, y_offset, part, color, true
-        #    part_offset += (part.length + 1) * FONT_WIDTH
-        #end
-        y_offset += FONT_HEIGH
-    end
-    cursor = $editor.showable_cursor
-    line cursor.x * FONT_WIDTH + 1, cursor.y * FONT_HEIGH, cursor.x * FONT_WIDTH + 1, cursor.y * FONT_HEIGH + FONT_HEIGH + 1, 8
+    y_offset += FONT_HEIGH
+  end
+  cursor = $editor.showable_cursor
+  line cursor.x * FONT_WIDTH + 1, cursor.y * FONT_HEIGH, cursor.x * FONT_WIDTH + 1, cursor.y * FONT_HEIGH + FONT_HEIGH + 1, 8
 end
--- programs/terminal.rb
+++ programs/terminal.rb
@@ -4,181 +4,181 @@ FONT_HEIGH = 6.freeze
 $terminal = nil
 
 class Window
-	def self.resize_event(new_width, new_height)
-		$terminal.update_showable(new_width, new_height)
-	end
+  def self.resize_event(new_width, new_height)
+    $terminal.update_showable(new_width, new_height)
+  end
 
-	def self.text_event(char)
-		$terminal.input_char(char)
-	end
+  def self.text_event(char)
+    $terminal.input_char(char)
+  end
 
-	def self.key_press_event(key)
-		$terminal.parse_key(key)
-	end
+  def self.key_press_event(key)
+    $terminal.parse_key(key)
+  end
 
-	def self.focus_event(has_focus)
-		$terminal.should_blink(has_focus)
-	end
+  def self.focus_event(has_focus)
+    $terminal.should_blink(has_focus)
+  end
 end
 
 class Vector
-    attr_accessor :x
-    attr_accessor :y
+  attr_accessor :x
+  attr_accessor :y
 
-    def initialize(x, y)
-        self.x = x
-        self.y = y
-    end
+  def initialize(x, y)
+    self.x = x
+    self.y = y
+  end
 
-    def +(other)
-		Vector.new(self.x + other.x, self.y + other.y)
-	end
+  def +(other)
+    Vector.new(self.x + other.x, self.y + other.y)
+  end
 
-    def -(other)
-		Vector.new(self.x - other.x, self.y - other.y)
-	end
+  def -(other)
+    Vector.new(self.x - other.x, self.y - other.y)
+  end
 
-    def to_s
-        "{x: #{self.x}, y: #{self.y}}"
-    end
+  def to_s
+    "{x: #{self.x}, y: #{self.y}}"
+  end
 end
 
 class Terminal
-	attr_reader :monospace
+  attr_reader :monospace
 
-	def initialize
-		Window.min_height = 7
-		Window.min_width = 9
-		@cursor = Vector.new(0, 0)
-		@blink = true
-		@blink_speed = 12
-		@blink_time = @blink_speed
-		@monospace = false
-		@return_value = false
-		@display_history = []
-		@command_history = []
-		@current_line = ''
-		@current_path = 'programs'
-		@should_blink = Window.focused
-		update_showable(Window.width, Window.height)
-	end
+  def initialize
+    Window.min_height = 7
+    Window.min_width = 9
+    @cursor = Vector.new(0, 0)
+    @blink = true
+    @blink_speed = 12
+    @blink_time = @blink_speed
+    @monospace = false
+    @return_value = false
+    @display_history = []
+    @command_history = []
+    @current_line = ''
+    @current_path = 'programs'
+    @should_blink = Window.focused
+    update_showable(Window.width, Window.height)
+  end
 
-	def update_showable(width, height)
-		@showable = Vector.new(width / FONT_WIDTH + 1, height / FONT_HEIGH + 1)
-        Window.title = "Terminal - #{@showable.x}x#{@showable.y}"
-	end
+  def update_showable(width, height)
+    @showable = Vector.new(width / FONT_WIDTH + 1, height / FONT_HEIGH + 1)
+    Window.title = "Terminal - #{@showable.x}x#{@showable.y}"
+  end
 
-	def should_blink(should)
-		@should_blink = should
-		@blink = true
-	end
+  def should_blink(should)
+    @should_blink = should
+    @blink = true
+  end
 
-	def showable_display
-		cursor = ''
-		@blink_time -= 1 if @should_blink
-		if @blink_time.zero?
-			@blink_time = @blink_speed
-			@blink = !@blink
-		end
-		cursor = '_' if @blink
-		(@display_history + [">#{@current_line}#{cursor}"])[[0, @display_history.count - @showable.y + 2].max, @showable.y]
-	end
+  def showable_display
+    cursor = ''
+    @blink_time -= 1 if @should_blink
+    if @blink_time.zero?
+      @blink_time = @blink_speed
+      @blink = !@blink
+    end
+    cursor = '_' if @blink
+    (@display_history + [">#{@current_line}#{cursor}"])[[0, @display_history.count - @showable.y + 2].max, @showable.y]
+  end
 
-	def input_char(char)
-		if char.between?(32, 126)
-            @current_line += char.chr
-		end
-	end
+  def input_char(char)
+    if char.between?(32, 126)
+      @current_line += char.chr
+    end
+  end
 
-	def parse_key(key)
-		case key
-		when 3 # d
-			Window.close if Window.key.include?(37)
-		when 17 # r
-			Window.reload if Window.key.include?(37)
-		when 58 # Enter
-			execute
-		when 59 # Backspace
-			@current_line.slice!(-1)
-		when 60 # Tab
-			# Pre-parse to auto-complete
-		end
-	end
+  def parse_key(key)
+    case key
+    when 3 # d
+      Window.close if Window.key.include?(37)
+    when 17 # r
+      Window.reload if Window.key.include?(37)
+    when 58 # Enter
+      execute
+    when 59 # Backspace
+      @current_line.slice!(-1)
+    when 60 # Tab
+      # Pre-parse to auto-complete
+    end
+  end
 
-	def execute
-		@command_history << @current_line
-		@display_history << ">#{@current_line}"
-		# Parse command here
-		command = @current_line.split(' ')
-		result = nil
-		case command[0]
-		when 'monospace'
-			value = command[1]
-			@monospace = param_to_bool(value) if param_is_bool(value) unless value.nil?
-			result = @monospace
-		when 'return_value'
-			value = command[1]
-			@return_value = param_to_bool(value) if param_is_bool(value) unless value.nil?
-			result = @return_value
-		when 'spawn'
-			result = Desktop.spawn "#{@current_path}/#{command[1]}", command.drop(2)
-		when 'history'
-			history = @command_history
-			history = @command_history[(@command_history.count - command[1].to_i)..@command_history.count] if command[1].to_i.is_a?(Integer) # TODO: Check working
-			history.each_with_index { |command, line| @display_history << "#{line + 1} #{command}" }
-		when 'cd'
-			if command[1].nil?
-				@current_path = 'programs'
-			else
-				@current_path += "/#{command[1]}"
-				paths = @current_path.split('/')
-				# Do magic with '..' and '.'
-				result = @current_path
-			end
-		when 'pwd'
-			result = @current_path
-			@display_history << result
-		when 'ps'
-			result = Desktop.processes
-			result.each { |process| @display_history << process }
-		when 'kill'
-			result = Desktop.kill_process(command[1].to_i) if command[1].to_i.is_a?(Integer)
-		when 'exit'
-			Window.close
-		end
-		@display_history << result.inspect if @return_value
-		@current_line = ''
-		@cursor.y += 1
-	end
+  def execute
+    @command_history << @current_line
+    @display_history << ">#{@current_line}"
+    # Parse command here
+    command = @current_line.split(' ')
+    result = nil
+    case command[0]
+    when 'monospace'
+      value = command[1]
+      @monospace = param_to_bool(value) if param_is_bool(value) unless value.nil?
+      result = @monospace
+    when 'return_value'
+      value = command[1]
+      @return_value = param_to_bool(value) if param_is_bool(value) unless value.nil?
+      result = @return_value
+    when 'spawn'
+      result = Desktop.spawn "#{@current_path}/#{command[1]}", command.drop(2)
+    when 'history'
+      history = @command_history
+      history = @command_history[(@command_history.count - command[1].to_i)..@command_history.count] if command[1].to_i.is_a?(Integer) # TODO: Check working
+      history.each_with_index { |command, line| @display_history << "#{line + 1} #{command}" }
+    when 'cd'
+      if command[1].nil?
+        @current_path = 'programs'
+      else
+        @current_path += "/#{command[1]}"
+        paths = @current_path.split('/')
+        # Do magic with '..' and '.'
+        result = @current_path
+      end
+    when 'pwd'
+      result = @current_path
+      @display_history << result
+    when 'ps'
+      result = Desktop.processes
+      result.each { |process| @display_history << process }
+    when 'kill'
+      result = Desktop.kill_process(command[1].to_i) if command[1].to_i.is_a?(Integer)
+    when 'exit'
+      Window.close
+    end
+    @display_history << result.inspect if @return_value
+    @current_line = ''
+    @cursor.y += 1
+  end
 
-	def param_is_bool(param)
-		param = param.downcase
-		if ['0', 'false', '1', 'true'].include?(param)
-			return true
-		end
-		false
-	end
+  def param_is_bool(param)
+    param = param.downcase
+    if ['0', 'false', '1', 'true'].include?(param)
+      return true
+    end
+    false
+  end
 
-	def param_to_bool(param)
-		param = param.downcase
-		if ['1', 'true'].include?(param)
-			return true
-		elsif ['0', 'false'].include?(param)
-			return false
-		end
-		nil
-	end
+  def param_to_bool(param)
+    param = param.downcase
+    if ['1', 'true'].include?(param)
+      return true
+    elsif ['0', 'false'].include?(param)
+      return false
+    end
+    nil
+  end
 end
 
 def init
-	$terminal = Terminal.new
+  $terminal = Terminal.new
 end
 
 def update
-	clear 5
-	y_offset = 0;
-	$terminal.showable_display.each do |line|
-		text 1, 1 + y_offset, line, 7, $terminal.monospace
-		y_offset += FONT_HEIGH
-	end
+  clear 5
+  y_offset = 0;
+  $terminal.showable_display.each do |line|
+    text 1, 1 + y_offset, line, 7, $terminal.monospace
+    y_offset += FONT_HEIGH
+  end
 end
