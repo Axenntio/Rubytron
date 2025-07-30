6abdaf4139441d67e7d2bde0acd9be684c588e06
Axenntio <axenntio@live.com>
2025-07-30 07:33:23 +0200

    Add missing dependancie and command change for windows

--- src/Runtime/meson.build.windows.sh
+++ src/Runtime/meson.build.windows.sh
@@ -4,5 +4,5 @@ EXECUTABLE="$1"
 OUTPUT="$2"
 
 mkdir windows
-mv $EXECUTABLE windows/.
-zip -r $OUTPUT windows
+mv $EXECUTABLE windows/runtime.exe
+Compress-Archive -Path "windows" -DestinationPath $OUTPUT
